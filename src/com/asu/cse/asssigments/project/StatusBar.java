514a43dea8392a6a91dc7a857a8ee294136a8323
disha4u <disha.97.agarwal@gmail.com>
2022-11-25 15:12:09 -0700

    added dummy data

--- src/com/asu/cse/asssigments/project/StatusBar.java
+++ src/com/asu/cse/asssigments/project/StatusBar.java
@@ -2,18 +2,25 @@ package com.asu.cse.asssigments.project;
 import java.awt.Color;
 import java.awt.Dimension;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class StatusBar extends JPanel implements Observer {
-
+	
+	Storage storage = Storage.getInstance();
+	JLabel label;
 	StatusBar()
 	{
 //		setSize(new Dimension(400, 400));
+		label = new JLabel();
+		add(label);
 		setBackground(Color.RED);
 	}
+	
 	@Override
 	public void Update(Observable o) {
 		// TODO Auto-generated method stub
+		int index = storage.getclasssize();
 		
 	}
 
