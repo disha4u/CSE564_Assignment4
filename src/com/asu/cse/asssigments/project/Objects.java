fb7d4d4ea77eff57c108e03824bb384800e6f7f3
disha4u <disha.97.agarwal@gmail.com>
2022-11-24 18:14:56 -0700

    added more storage code

--- src/com/asu/cse/asssigments/project/Objects.java
+++ src/com/asu/cse/asssigments/project/Objects.java
@@ -2,4 +2,9 @@ package com.asu.cse.asssigments.project;
 public class Objects {
  String name;
  double xcoor,ycoor;
+ Objects(String s,double x,double y){
+	 this.name=s;
+	 this.xcoor=x;
+	 this.ycoor=y;
+ }
 }
