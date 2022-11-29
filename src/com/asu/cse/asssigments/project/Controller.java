478c87815ef58194f76b23257383342a7997c1f1
Harshit Viren Shah <hvshah97@gmail.com>
2022-11-28 18:18:10 -0700

    Added ActionListener

--- src/com/asu/cse/asssigments/project/Controller.java
+++ src/com/asu/cse/asssigments/project/Controller.java
@@ -1,4 +1,18 @@
 package com.asu.cse.asssigments.project;
-public class Controller {
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Controller implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String action = e.getActionCommand();
+		FileOperations operations = new FileOperations();
+		if(action.equals("Save")) {
+			operations.tofile("Text.txt");
+		}
+		
+	}
+	
 }
