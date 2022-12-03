a43cecb09dbffec5ccc1d4848ff06162994dbcc0
Chandana <cgollapu@asu.edu>
2022-12-02 17:24:11 -0700

    Help info

--- src/com/asu/cse/asssigments/project/Help.java
+++ src/com/asu/cse/asssigments/project/Help.java
@@ -4,11 +4,13 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Help extends JFrame{
-	Help(){
+public class Help extends JFrame {
+	Help() {
 		JPanel panel = new JPanel();
-		JLabel label = new JLabel("<html>Harshit Viren Shah: hshah78@asu.edu<br/>Janaki Venkata Ramchandra Sai Nayani: jnayani@asu.edu<br />Disha Agrawal: dagarw24@asu.edu</html>");
-        panel.add(label);
-        add(panel);
+		JLabel label = new JLabel(
+				"<html><br /> Harshit Viren Shah: hshah78@asu.edu<br/>Janaki Venkata Ramchandra Sai: jnayani@asu.edu<br />Disha Agrawal: dagarw24@asu.edu <br /> Vinaykumar Chalamalla: vchalama@asu.edu <br /> Chandana Gollapudi: cgollapu@asu.edu <br /> Karthikeya Sai Boggarapu: kboggara@asu.edu </html>");
+		
+		panel.add(label);
+		add(panel);
 	}
 }
