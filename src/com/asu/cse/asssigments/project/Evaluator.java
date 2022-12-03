054e7dac2056c7dc01b8e82bbb76937decc47a96
disha4u <disha.97.agarwal@gmail.com>
2022-12-02 18:17:12 -0700

    added and

--- src/com/asu/cse/asssigments/project/Evaluator.java
+++ src/com/asu/cse/asssigments/project/Evaluator.java
@@ -7,7 +7,7 @@ public class Evaluator {
 		for(int i=0;i<s.getclasssize();i++) {
 			double x1=s.readclass(i).getx();
 			double y1=s.readclass(i).gety();
-			if(Math.abs(x-x1)<BoxDimesions.length/2 || Math.abs(y-y1)<BoxDimesions.width/2) {
+			if(Math.abs(x-x1)<BoxDimesions.length/2 && Math.abs(y-y1)<BoxDimesions.width/2) {
 				result = s.readclass(i).getname();
 				break;
 			}
